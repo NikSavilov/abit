8825d521f10d18b3be9a1f484960488eb5737301
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-02-25 18:31:09 +0300

    Try to send pdf

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import requests, time, os, httplib2, random, base64, zipfile, pdfkit
+import requests, time, os, httplib2, random, base64, zipfile, pdfkit, sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,6 +6,10 @@ from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
+
+SCOPES = 'https://mail.google.com/'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -71,32 +75,47 @@ def unzip(files_list):
 		zip_pack.close()		
 	return unzip_list
 		
-def load_image(unzip_list):
+def load_to_pdf(unzip_list):
 	url = "https://rosreestr.ru/wps/portal/p/cc_ib_portal_services/cc_vizualisation/"
-	chromeOptions = webdriver.ChromeOptions() 
-	prefs = { 
-	"browser.helperApps.alwaysAsk.force" :False, 
-	"browser.helperApps.neverAsk.saveToDisk" : "application/pdf;text/plain;text/csv"} 
-	chromeOptions.add_experimental_option("prefs",prefs)
+	pdf_list = []
 	driver = webdriver.Chrome('chromedriver.exe')
 	for path in unzip_list:
 		driver.get(url) 
 		time.sleep(1)
 		upload_box = driver.find_element(By.ID,"xml_file");
-		upload_box.send_keys('C:/code/abit/' + path)
+		upload_box.send_keys(path)
 		driver.find_element(By.CLASS_NAME,"terminal-button-bright").click()
 		link_load = driver.find_element(By.XPATH, '/html/body/div/section/div[1]/table/tbody/tr/td/div[2]/div/div/div/div/table/tbody/tr[3]/td/form/table/tbody/tr[3]/td[2]/table/tbody/tr/td/table/tbody/tr/td[2]/a')
 		link_load.click()
 		driver.switch_to.window(driver.window_handles[-1]);
 		page = driver.page_source
 		soup = BeautifulSoup(page,'html5lib')
-		
-
+		div_to_del = soup.find('div', attrs = {'class': 'noprint'})
+		page = page.replace(div_to_del,'') 
 		name , _ = os.path.splitext(path)
 		pdfkit.from_string(page,name + '.pdf')
+		pdf_list.append(name + '.pdf')
+	return pdf_list
+
+def send_pdf(pdf_list, to='elen-savilova@yandex.com'):
+	driver = webdriver.Chrome('chromedriver')
+	driver.get('https://mail.google.com/mail/ca/u/0/h/')
+	driver.find_element(By.XPATH,'/html/body/table[2]/tbody/tr/td[1]/table[1]/tbody/tr[1]/td/b/a').click()
+	driver.find_element(By.XPATH,'//*[@id="to"]').send_keys(to)
+	driver.find_element(By.XPATH,'/html/body/table[2]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/form/table[2]/tbody/tr[4]/td[2]/input').send_keys('FILE')
+	driver.find_element(By.XPATH,'/html/body/table[3]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/form/table[2]/tbody/tr[7]/td[2]/input').click()
+	num = 1
+	for pdf in pdf_list:
+		path = 'C:/code/arb_conv' + pdf 
+		driver.find_element(By.XPATH,'/html/body/table[3]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/table[3]/tbody/tr[{num}}]/td[2]/input'.format(num = num)).send_keys(path)
+		num = num + 1
+	driver.find_element(By.XPATH,'/html/body/table[3]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/table[4]/tbody/tr/td/input[1]').click()
+	driver.find_element(By.XPATH,'/html/body/table[3]/tbody/tr/td[2]/table[1]/tbody/tr/td[2]/table[4]/tbody/tr/td/input[1]').click()
 
 
-		
-files = mail_parser()
-unzip_list = unzip(files)
-load_image(unzip_list)
+
+
+#files = mail_parser()
+#unzip_list = unzip(files)
+#pdf_list = load_to_pdf(unzip_list)
+send_pdf(['/loaded/unzip/1.pdf'])
